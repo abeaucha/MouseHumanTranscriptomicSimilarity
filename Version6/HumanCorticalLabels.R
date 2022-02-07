45a13c7ea730716df4f13d125dac0c753851bedf
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-02-07 12:54:41 -0500

    Updated human cortical label script to include leaf node lateral labels

--- Version6/HumanCorticalLabels.R
+++ Version6/HumanCorticalLabels.R
@@ -25,22 +25,41 @@ load("/projects/abeauchamp/Projects/MouseHumanMapping/AllenHumanBrainAtlas/Data/
 treeHuman <- Clone(treeHumanExpr)
 rm(treeHumanExpr)
 
+#Get all cortical leaf nodes
+corticalRegionsAll <- FindNode(treeHuman, "cerebral cortex")$Get("name", filterFun = isLeaf)
+
+#Create a copy of the tree to prune down to be bilateral
+treeHumanBilateral <- Clone(treeHuman)
+
 #Remove white matter and ventricles from the tree
-pruneAnatTree(treeHuman, nodes = c("white matter", "sulci & spaces"), method = "AtNode")
+pruneAnatTree(treeHumanBilateral, nodes = c("white matter", "sulci & spaces"), method = "AtNode")
 
 #Prune the tree down to 166 bilateral regions
-pruneAnatTree(treeHuman, nodes = listLabelsHuman$Region166, method = "BelowNode")
+pruneAnatTree(treeHumanBilateral, nodes = listLabelsHuman$Region166, method = "BelowNode")
 
 #Extract cortical regions from the tree
-corticalRegions <- FindNode(treeHuman, "cerebral cortex")$Get("name", filterFun = isLeaf)
+corticalRegionsBilateral <- FindNode(treeHumanBilateral, "cerebral cortex")$Get("name", filterFun = isLeaf)
 
-#Filter sample labels based on cortical regions
-dfCorticalRegions <- dfExprHuman %>% 
+#Empty data frame to match lateral leaf nodes with bilateral parent regions
+dfCorticalRegionsAll <- tibble(RegionAll = corticalRegionsAll,
+                               Region166 = "")
+
+#Match leaf nodes with bilateral parents
+for (i in 1:nrow(dfCorticalRegionsAll)){
+  cxRegion <- dfCorticalRegionsAll$RegionAll[[i]]
+  cxRegionPath <- FindNode(treeHuman, cxRegion)$path
+  indBilateral <- which(cxRegionPath %in% corticalRegionsBilateral)
+  dfCorticalRegionsAll[i,"Region166"] <- cxRegionPath[indBilateral]  
+}
+
+#Combine cortical labels at multiple levels with lateral leaf nodes
+dfCorticalRegions <- dfExprHuman %>%
   select(Region16, Region88, Region166) %>% 
   distinct() %>% 
-  filter(Region166 %in% corticalRegions) %>% 
-  mutate(Region166 = factor(Region166, levels = corticalRegions)) %>% 
-  arrange(Region166)
+  filter(Region166 %in% corticalRegionsBilateral) %>% 
+  left_join(dfCorticalRegionsAll, by = "Region166") %>% 
+  mutate(RegionAll = factor(RegionAll, levels = corticalRegionsAll)) %>% 
+  arrange(RegionAll)
 
 #Save cortical labels as csv file
 write_csv(x = dfCorticalRegions,
