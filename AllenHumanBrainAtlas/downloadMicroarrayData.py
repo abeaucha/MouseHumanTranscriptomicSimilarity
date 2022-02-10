1653efab8f3e418b5a4c3b5ca1de860d2fe1f92b
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-02-10 14:09:34 -0500

    Minor comment updates to microarray download script

--- AllenHumanBrainAtlas/downloadMicroarrayData.py
+++ AllenHumanBrainAtlas/downloadMicroarrayData.py
@@ -1,13 +1,22 @@
+# downloadMicroarrayData.py 
+#
+# Download AHBA microarray data from the web API
+# 
+# Antoine Beauchamp
+# Created: February 10th, 2022
+# Edited: February 10th, 2022
+
 #Packages
 import os
 import abagen
 
-#Directory to store data
+#Directory to store the data
 data_dir = os.getcwd()+'/Data/microarray'
 
-#If the "microarray" directory does not exist, create it
-#abagen will download to ~ if this does not exist
+#If the "microarray" sub-directory does not exist, create it
+#abagen will download to ~ otherwise
 if not os.path.exists(data_dir):
     os.mkdir(data_dir)
-   
+
+#Download microarray data
 files = abagen.fetch_microarray(donors = 'all', data_dir = data_dir)
