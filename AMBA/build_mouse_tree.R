f9947ac35a7c5851c89b3769637bc89c5bdbc6d7
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-03-09 14:32:49 -0500

    Updated amba tree script

--- AMBA/build_mouse_tree.R
+++ AMBA/build_mouse_tree.R
@@ -4,26 +4,20 @@
 # Antoine Beauchamp
 # Created: January 31st, 2022
 # Edited March 8th, 2022
-# ---------------------------------------------------------------------------------------
-
 
 message("Initializing...")
 
 #Libraries
-library(tidyverse)
+suppressPackageStartupMessages(library(tidyverse))
 library(data.tree)
 library(rjson)
 
-
 #Import tree functions
-source("../functions/abi_parsing_funs.R")
+source("../functions/tree_tools.R")
 
 #Import expression data
-
 message("Importing expression data...")
-
-# dfExpr <- read_csv("data/<file>")
-dfExpr <- read_csv(str_c(pathAMBA, "Data/", "MouseExpressionMatrix_ROI_DSURQE_coronal_maskcoronal.csv"))
+dfExpr <- suppressMessages(read_csv("data/MouseExpressionMatrix_ROI_DSURQE_coronal_maskcoronal_log2_grouped_imputed.csv"))
 
 message("Generating expression tree...")
 
@@ -32,7 +26,7 @@ treeDefs <- parse_abi_hierarchy("data/DSURQE_tree.json")
 treeMouseExpr <- Clone(treeDefs)
 
 #Import DSURQE atlas definitions
-defs <- read_csv("data/imaging/DSURQE_40micron_R_mapping_long.csv")
+defs <- suppressMessages(read_csv("data/imaging/DSURQE_40micron_R_mapping_long.csv"))
 
 #Assign DSURQE ROI expression values to leaf nodes on the tree
 treeMouseExpr$Do(function(node){
@@ -57,5 +51,5 @@ treeMouseExpr$Do(function(node){
 message("Writing to file...")
 
 save(treeMouseExpr, 
-     file = "data/MouseExpressionTree_DSURQE.RData"))
+     file = "data/MouseExpressionTree_DSURQE.RData")
 
