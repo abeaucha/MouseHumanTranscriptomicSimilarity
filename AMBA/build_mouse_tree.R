7a337bb25905f2948557397a9439f5b6ffcda71d
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-03-08 22:07:17 -0500

    Updated paths

--- AMBA/build_mouse_tree.R
+++ AMBA/build_mouse_tree.R
@@ -1,9 +1,9 @@
-# BuildMouseExprTree.R ------------------------------------------------------------------
+# build_mouse_tree.R ------------------------------------------------------------------
 # 
 #
 # Antoine Beauchamp
 # Created: January 31st, 2022
-# Edited: January 31st, 2022
+# Edited March 8th, 2022
 # ---------------------------------------------------------------------------------------
 
 
@@ -14,28 +14,25 @@ library(tidyverse)
 library(data.tree)
 library(rjson)
 
-#Paths
-pathAMBA <- "/projects/abeauchamp/Projects/MouseHumanMapping/AllenMouseBrainAtlas/"
-pathAtlas <- "/projects/abeauchamp/Atlases/DSURQE/"
 
 #Import tree functions
-source("/projects/abeauchamp/Functions/abi_parsing_funs.R")
+source("../functions/abi_parsing_funs.R")
 
 #Import expression data
 
 message("Importing expression data...")
 
+# dfExpr <- read_csv("data/<file>")
 dfExpr <- read_csv(str_c(pathAMBA, "Data/", "MouseExpressionMatrix_ROI_DSURQE_coronal_maskcoronal.csv"))
 
 message("Generating expression tree...")
 
 #Import DSURQE/AMBA tree from JSON
-treeJSON <- "DSURQE_tree.json"
-treeDefs <- parse_abi_hierarchy(str_c(pathAtlas, treeJSON))
+treeDefs <- parse_abi_hierarchy("data/DSURQE_tree.json")
 treeMouseExpr <- Clone(treeDefs)
 
 #Import DSURQE atlas definitions
-defs <- read_csv(str_c(pathAtlas, "DSURQE_40micron_R_mapping_long.csv"))
+defs <- read_csv("data/imaging/DSURQE_40micron_R_mapping_long.csv")
 
 #Assign DSURQE ROI expression values to leaf nodes on the tree
 treeMouseExpr$Do(function(node){
@@ -60,5 +57,5 @@ treeMouseExpr$Do(function(node){
 message("Writing to file...")
 
 save(treeMouseExpr, 
-     file = str_c(pathAMBA, "Data/", "MouseExpressionTree_DSURQE.RData"))
+     file = "data/MouseExpressionTree_DSURQE.RData"))
 
