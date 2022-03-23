5997bc6da21ba9cb536ccd5f5ffde7a16f9919fe
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-03-23 13:44:27 -0400

    build_sample_matrix.R: Updated documentation

--- AHBA/build_sample_matrix.R
+++ AHBA/build_sample_matrix.R
@@ -1,93 +1,102 @@
-# BuildSampleExprMatrix.R ---------------------------------------------------------------
-#
-# This is a script to process the Allen Human Brain Atlas microarray gene expression 
-# data and build a gene-by-sample expression matrix. The processing steps are loosely
-# based on the paper "A practical guide to linking brain-wide gene expression and 
-# neuroimaging data" by Aurina Arnatkeviciute, published in NeuroImage, 2019. 
-# Two versions of the pipeline are available
-#
-# Output:
-#
-# Antoine Beauchamp
+# ----------------------------------------------------------------------------
+# build_sample_matrix.R
+# Author: Antoine Beauchamp
 # Created: August 20th, 2021
-# Edited: March 5th, 2022
+#
+# Description
+# -----------
+# This is a script to process the Allen Human Brain Atlas microarray gene 
+# expression data and build a gene-by-sample expression matrix. 
+# The processing steps are loosely based on the paper 
+# "A practical guide to linking brain-wide gene expression and 
+# neuroimaging data" by Aurina Arnatkeviciute, published in NeuroImage, 2019. 
+# Two versions of the pipeline are available.
 
 
-# Libraries -----------------------------------------------------------------------------
+# Packages -------------------------------------------------------------------
+
+suppressPackageStartupMessages(library(optparse))
 suppressPackageStartupMessages(library(tidyverse))
-library(optparse)
 
 
-# Command line arguments ----------------------------------------------------------------
+# Command line arguments -----------------------------------------------------
 
 option_list <- list(
   make_option("--version",
               type = "numeric",
               default = 1,
-              help = "Version of the data processing pipeline to run [default %default]"),
+              help = paste("Version of the data processing pipeline to run",
+                           "[default %default]")),
   make_option("--verbose",
               type = "character",
               default = "true",
-              help = "Verbose option for data processing pipeline [default %default]")
+              help = paste("Verbose option for data processing pipeline",
+                           "[default %default]"))
 )
 
 args <- parse_args(OptionParser(option_list = option_list))
 
 if (!(args[["version"]] %in% c(1,2))){
-  stop(str_c("Argument --version must be one of [1, 2] (got ", args[["version"]], ")"))
+  stop(paste("Argument --version must be one of [1, 2]",
+             "(got", args[["version"]], ")"))
 }
 
 if (!(args[["verbose"]] %in% c("true","false"))){
-  stop(str_c("Argument --verbose must be one of [true, false] (got ", args[["verbose"]], ")"))
+  stop(paste("Argument --verbose must be one of [true, false]",
+             "(got", args[["verbose"]], ")"))
 }
 
 
-# Paths ---------------------------------------------------------------------------------
+# Functions ------------------------------------------------------------------
+
+source("processing_tools_AHBA.R")
+
+
+# Import ---------------------------------------------------------------------
 
 #Set paths to AHBA directories
 pathData <- "data/microarray/"
 donorDirectories <- list.files(pathData)
 
-
-# Functions -----------------------------------------------------------------------------
-
-source("functions/processing_tools.R")
-
-
-# Import --------------------------------------------------------------------------------
-
 message("Importing data...")
 
 #Import donor information and naming conventions
 dfDonorInfo <- suppressMessages(read_csv("data/donors.csv")) %>% 
-  inner_join(tibble(donorFileID = str_remove(donorDirectories, "normalized_microarray_"),
-                    donorPath = str_c(pathData, donorDirectories, "/")),
+  inner_join(tibble(donorFileID = str_remove(donorDirectories, 
+                                             "normalized_microarray_"),
+                    donorPath = str_c(pathData, 
+                                      donorDirectories, 
+                                      "/")),
              by = "donorFileID") %>% 
   select(-donorFileID)
- 
+
 #Import data for each donor
-listImport <- map2(dfDonorInfo$donorPath, dfDonorInfo$donorID, importDonorData)
+listImport <- map2(dfDonorInfo$donorPath, 
+                   dfDonorInfo$donorID,
+                   importDonorData)
 names(listImport) <- dfDonorInfo$donorID
 
 
-# Pipeline ------------------------------------------------------------------------------
+# Pipeline -------------------------------------------------------------------
 
 #Apply processing pipeline to the data
 listData <- processingPipeline(listImport,
                                version = args[["version"]],
-                               verbose = ifelse(args[["verbose"]] == "true", TRUE, FALSE))
+                               verbose = ifelse(args[["verbose"]] == "true", 
+                                                TRUE,
+                                                FALSE))
 
 
-# Write ---------------------------------------------------------------------------------
+# Write ----------------------------------------------------------------------
 
 message("Writing to file...")
 
 #Write gene expression matrix
-exprfile <- str_c("data/", "HumanExpressionMatrix_Samples_pipeline_v", args[["version"]], ".csv")
+exprfile <- paste0("HumanExpressionMatrix_samples_pipeline_v", args[["version"]], ".csv")
 write_csv(x = listData[["GeneExpression"]],
-          file = exprfile)
+          file = paste0("data/", exprfile))
 
 #Write sample information
-samplefile <- str_c("data/", "SampleInformation_pipeline_v", args[["version"]], ".csv")
+samplefile <- paste0("SampleInformation_pipeline_v", args[["version"]], ".csv")
 write_csv(x = listData[["SampleInfo"]],
-          file = samplefile)
+          file = paste0("data/", samplefile))
