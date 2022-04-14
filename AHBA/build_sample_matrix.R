8f37227cc5863ad091b36be7cad7df943d6f6b67
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-04-14 14:10:56 -0400

    Added verbosity option and updated string formatting

--- AHBA/build_sample_matrix.R
+++ AHBA/build_sample_matrix.R
@@ -21,45 +21,45 @@ suppressPackageStartupMessages(library(optparse))
 # Command line arguments -----------------------------------------------------
 
 option_list <- list(
-  make_option("--microarraydir",
-              type = "character",
-              default = "data/microarray/",
+  make_option('--microarraydir',
+              type = 'character',
+              default = 'data/microarray/',
               help = paste("Directory containing the microarray data sets",
                            "from the Allen Human Brain Atlas", 
                            "[default %default]")),
-  make_option("--datadir",
-              type = "character",
-              default = "data/", 
+  make_option('--datadir',
+              type = 'character',
+              default = 'data/', 
               help = paste("Directory containing additional data files",
                            "This also acts as the output directory",
                            "[default %default]")),
-  make_option("--donorsfile",
-              type = "character",
-              default = "donors.csv",
+  make_option('--donorsfile',
+              type = 'character',
+              default = 'donors.csv',
               help = paste("CSV file containing donor name conventions",
                            "[default %default]")),
-  make_option("--version",
-              type = "numeric",
+  make_option('--version',
+              type = 'numeric',
               default = 1,
               help = paste("Version of the data processing pipeline to run",
                            "[default %default]")),
-  make_option("--verbose",
-              type = "character",
-              default = "true",
+  make_option('--verbose',
+              type = 'character',
+              default = 'true',
               help = paste("Verbose option for data processing pipeline",
                            "[default %default]"))
 )
 
 args <- parse_args(OptionParser(option_list = option_list))
 
-if (!(args[["version"]] %in% c(1,2))){
+if (!(args[['version']] %in% c(1,2))){
   stop(paste("Argument --version must be one of [1, 2]",
-             "(got", args[["version"]], ")"))
+             "(got", args[['version']], ")"))
 }
 
-if (!(args[["verbose"]] %in% c("true","false"))){
+if (!(args[['verbose']] %in% c('true','false'))){
   stop(paste("Argument --verbose must be one of [true, false]",
-             "(got", args[["verbose"]], ")"))
+             "(got", args[['verbose']], ")"))
 }
 
 
@@ -68,35 +68,36 @@ if (!(args[["verbose"]] %in% c("true","false"))){
 working_dir <- getwd()
 
 script_dir <- commandArgs() %>% 
-  str_subset("--file=") %>% 
-  str_remove("--file=") %>% 
+  str_subset('--file=') %>% 
+  str_remove('--file=') %>% 
   dirname()
 
-path_tools <- str_c(working_dir, script_dir, "processing_tools_AHBA.R",
-                    sep = "/")
+path_tools <- str_c(working_dir, script_dir, 'processing_tools_AHBA.R',
+                    sep = '/')
 
 source(path_tools)
 
 
 # Import ---------------------------------------------------------------------
 
-dirMicroarray <- args[["microarraydir"]]
-dirData <- args[["datadir"]]
-fileDonors <- args[["donorsfile"]]
+dirMicroarray <- args[['microarraydir']]
+dirData <- args[['datadir']]
+fileDonors <- args[['donorsfile']]
+verbose <- ifelse(args[['verbose']] == 'true', TRUE, FALSE)
 
 #Set paths to AHBA directories
 donorDirectories <- list.files(dirMicroarray)
 
-message("Importing data...")
+if (verbose) {message("Importing data...")}
 
 #Import donor information and naming conventions
 dfDonorInfo <- suppressMessages(read_csv(str_c(dirData, fileDonors))) %>% 
   inner_join(tibble(donorFileID = str_remove(donorDirectories, 
-                                             "normalized_microarray_"),
+                                             'normalized_microarray_'),
                     donorPath = str_c(dirMicroarray, 
                                       donorDirectories, 
-                                      "/")),
-             by = "donorFileID") %>% 
+                                      '/')),
+             by = 'donorFileID') %>% 
   select(-donorFileID)
 
 #Import data for each donor
@@ -110,22 +111,20 @@ names(listImport) <- dfDonorInfo$donorID
 
 #Apply processing pipeline to the data
 listData <- processingPipeline(listImport,
-                               version = args[["version"]],
-                               verbose = ifelse(args[["verbose"]] == "true", 
-                                                TRUE,
-                                                FALSE))
+                               version = args[['version']],
+                               verbose = verbose)
 
 
 # Write ----------------------------------------------------------------------
 
-message("Writing to file...")
+if (verbose) {message("Writing to file...")}
 
 #Write gene expression matrix
-exprfile <- paste0("HumanExpressionMatrix_samples_pipeline_v", args[["version"]], ".csv")
-write_csv(x = listData[["GeneExpression"]],
+exprfile <- paste0('HumanExpressionMatrix_samples_pipeline_v', args[['version']], '.csv')
+write_csv(x = listData[['GeneExpression']],
           file = paste0(dirData, exprfile))
 
 #Write sample information
-samplefile <- paste0("SampleInformation_pipeline_v", args[["version"]], ".csv")
-write_csv(x = listData[["SampleInfo"]],
+samplefile <- paste0('SampleInformation_pipeline_v', args[['version']], '.csv')
+write_csv(x = listData[['SampleInfo']],
           file = paste0(dirData, samplefile))
