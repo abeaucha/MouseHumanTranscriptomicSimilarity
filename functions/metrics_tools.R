089acc2cc32ebf1e8c97db1cc10719ecd64994cc
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-04-20 12:37:18 -0400

    Updated documentation

--- functions/metrics_tools.R
+++ functions/metrics_tools.R
@@ -1,9 +1,13 @@
 #' Compute the cosine similarity
 #'
+#' @description Compute the cosine similarity between the columns of 
+#' a matrix or two matrices.
+#'
 #' @param x1 (matrix) A numeric matrix 
 #' @param x2 (matrix) A numeric matrix
 #'
-#' @return A square matrix containing the cosine similarity between the columns of m1 (and m2)
+#' @return (matrix) A matrix containing the cosine similarity between
+#' the columns of m1 (and m2)
 cosineSimilarity <- function(x1, x2=NULL){
   
   if(is.null(x2)){
@@ -21,18 +25,23 @@ cosineSimilarity <- function(x1, x2=NULL){
 
 #' Compute the Euclidean distance
 #'
-#' @param x A numeric matrix
+#' @description Compute the Euclidean distance between the columns of 
+#' a matrix.
 #'
-#' @return A square matrix containing the Euclidean distance between all columns
+#' @param x (matrix) A numeric matrix
+#'
+#' @return (matrix) A square matrix containing the Euclidean distance 
+#' between all columns
 distEuclidean <- function(x){as.matrix(dist(t(x), method = "euclidean"))}
 
 #' Extract a sub-matrix
 #'
-#' @param x 
-#' @param rowInd 
-#' @param colInd 
+#' @param x (matrix)
+#' @param rowInd (logical vector) Which rows to extract.
+#' @param colInd (logical vector) Which columns to extract.
 #'
-#' @return
+#' @return (matrix) The sub-matrix identified by the provided rows and
+#' columns.
 extractSubMat <- function(x, rowInd, colInd = NULL){
   if(is.null(colInd)){colInd <- rowInd}
   return(x[rowInd, colInd])
