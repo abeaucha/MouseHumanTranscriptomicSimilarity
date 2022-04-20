beeb6dccceedc829a6d75d1dcfdc9970e29ba778
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-04-20 13:05:59 -0400

    Updated roxygen headers

--- functions/tree_tools.R
+++ functions/tree_tools.R
@@ -1,12 +1,11 @@
-#' Check if object is a data tree
-#'
-#' @description 
-#' `isTree` checks whether its argument is a data tree
+#' Check whether object is a data tree
 #'
 #' @param x Object to test for tree structure
 #' 
-#' @return Boolean value TRUE or FALSE
-isTree <- function(x){any(class(x) == "Node") & any(class(x) == "R6")}
+#' @return (logical scalar) TRUE or FALSE
+isTree <- function(x){
+  any(class(x) == "Node") & any(class(x) == "R6")
+}
 
 
 parse_abi_hierarchy <-
@@ -35,6 +34,7 @@ recursive_build_tree <- function(tree_list){
   }
 }
 
+
 #' Attach cut points to a tree node
 #' 
 #' @description 
@@ -42,72 +42,81 @@ recursive_build_tree <- function(tree_list){
 #' points are used by the pruning functions pruneAtNode and 
 #' pruneBelowNode to prune the tree based on those nodes.
 #' 
-#' @param node A data tree node 
-#' @param cutPoints A character vector containing the names of tree
-#'                  nodes to use as cut points
+#' @param node (Node) A data tree node 
+#' @param cutPoints (character vector) The names of tree nodes to use 
+#' as cut points.
 #'                  
 #' @return NULL                
-attachCutPoints <- function(node, cutPoints){node[["cutPoints"]] <- cutPoints}
-
+attachCutPoints <- function(node, cutPoints){
+  node[["cutPoints"]] <- cutPoints
+}
 
 
 #' Prune data tree at node
 #' 
 #' @description 
+#' Prune the data tree at the specified node name.
+#' 
+#' @details 
 #' This pruning function evaluates whether the name of the node is
-#' in the cut points (stored in the node field "cutPoints"). When 
+#' in the cut points (stored in the node field `cutPoints`). When 
 #' passed to the `pruneFun` argument in the `Prune` function from 
 #' `data.tree` it will prune the data tree at the nodes specified 
-#' in the "cutPoints" field.
+#' in the `cutPoints`` field.
 #'
-#' @param node A data tree node
+#' @param node (Node) A data tree node.
 #' 
-#' @return Boolean value TRUE or FALSE 
+#' @return (logical scalar) TRUE or FALSE 
 pruneAtNode <- function(node){
   
   if(is.null(node[["cutPoints"]])){
-    stop(paste("Node", node[["name"]], "has no field 'cutPoints'. Cut points can be attached using the function attachCutPoints()."))
+    stop(paste("Node", node[["name"]], "has no field 'cutPoints'.",
+               "Cut points can be attached using the function",
+               "attachCutPoints()."))
   }
   
   return(!(node$name %in% node$cutPoints))
 }
 
 
-
 #' Prune data tree below node
 #' 
 #' @description 
+#' Prune the data tree below the specified node name.
+#' 
+#' @details 
 #' This pruning function evaluates whether the node is a child of 
 #' the cut points (stored in the node field "cutPoints"). When 
 #' passed to the `pruneFun` argument in the `Prune` function from 
 #' `data.tree` it will prune the data tree below the nodes specified
 #' in the "cutPoints" field.
 #'
-#' @param node A data tree node
+#' @param node (Node) A data tree node.
 #' 
-#' @return Boolean value TRUE or FALSE 
+#' @return (logical scalar) TRUE or FALSE 
 pruneBelowNode <- function(node){
   
   if(is.null(node[["cutPoints"]])){
-    stop(paste("Node", node[["name"]], "has no field 'cutPoints'. Cut points can be attached using the function attachCutPoints()."))
+    stop(paste("Node", node[["name"]], "has no field 'cutPoints'.",
+               "Cut points can be attached using the function",
+               "attachCutPoints()."))
   }
   
   return(!any(node$cutPoints %in% node$path[-length(node$path)]))
   }
 
 
-
 #' Prune a tree based on node names
 #' 
 #' @description 
 #' 
-#' @param anatTree The data tree to prune
-#' @param nodes A character vector of node names to use to prune the
-#'              tree
-#' @param method One of "AtNode" or "BelowNode" indicating whether
-#'               to prune the tree at or below the nodes specified              
+#' @param anatTree The data tree to prune.
+#' @param nodes (character vector) Node names to use to prune the tree.
+#' @param method (character scalar) One of 'AtNode' or 'BelowNode' 
+#' indicating whether to prune the tree at or below the nodes specified.              
 #' 
-#' @return 
+#' @return (character scalar) Message indicating how the tree was 
+#' pruned.
 pruneAnatTree <- function(anatTree, nodes, method = "AtNode"){
   
   require(data.tree)
@@ -118,7 +127,8 @@ pruneAnatTree <- function(anatTree, nodes, method = "AtNode"){
   } else if (method == "BelowNode"){
     pruningFunction <- pruneBelowNode
   } else {
-    stop(paste("Argument `method` must be one of 'AtNode' or 'BelowNode'. Got", method))
+    stop(paste("Argument `method` must be one of 'AtNode' or 'BelowNode'.",
+               "Got", method))
   }
   
   #Attach cut points to tree
@@ -135,8 +145,17 @@ pruneAnatTree <- function(anatTree, nodes, method = "AtNode"){
 }
 
 
-
-## Function: hanatToAtlas -------------------------------------------
+#' Create an atlas from a tree
+#' 
+#' @description 
+#' Create an atlas in MINC format from the leaf nodes of a tree.
+#'
+#' @param anatTree A data tree.
+#' @param labelVolume (mincSingleDim) 3-dimensional array containing
+#' the all labels in the `label` field at the root level of `anatTree`.
+#'
+#' @return (mincSingleDim) 1-dimensional array containing the atlas 
+#' labels.
 hanatToAtlas <- function(anatTree, labelVolume){
   
   require(RMINC)
@@ -156,18 +175,16 @@ hanatToAtlas <- function(anatTree, labelVolume){
 }
 
 
-## Function: hanatToAtlasDefs ------------------------------------
-#
-# Generates new atlas definitions based on a pruned anatomical 
-# tree.
-#
-# Arguments: 
-# anatTree: The pruned tree for which to generate an atlas. Must
-#           have fields `label` and `label.new`.
-#
-# Return:
-# A data frame giving the new label definitions
-# -------------------------------------------------------------------
+#' Create atlas definitions.
+#'
+#' @description 
+#' Generate atlas definitions based on the leaf nodes of a
+#' neuroanatomical tree.
+#'
+#' @param anatTree A data tree.
+#'
+#' @return (data.frame) A data frame containing the definitions of the 
+#' atlas labels associated with the leaf nodes of `anatTree`.
 hanatToAtlasDefs <- function(anatTree){
   
   require(data.tree)
