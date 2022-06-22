b0774862e0d9bdeb6069d9d5f9587741e8f017ae
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-06-22 16:11:52 -0400

    Trying to obtain all samples from abagen workflow

--- AHBA/build_sample_matrix.py
+++ AHBA/build_sample_matrix.py
@@ -196,7 +196,11 @@ def main():
                                                     sim_threshold = sim_threshold,
                                                     sample_norm = sample_norm,
                                                     gene_norm = gene_norm,
+#                                                     corrected_mni = False, #This breaks the code. I think this is also where samples get filtered. 
                                                     verbose = verbose_int)
+    
+    print(expression.shape)
+    quit()
     
     if verbose:
         print("Getting sample metadata...")
