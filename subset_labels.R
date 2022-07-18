5e337f351b2afc871568527f3088aaeca3513e81
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-07-18 16:11:16 -0400

    Added code to extract isocortical voxels for unscaled data

--- subset_labels.R
+++ subset_labels.R
@@ -1,10 +1,38 @@
-library(tidyverse)
+suppressPackageStartupMessages(library(tidyverse))
 
 infile <- 'data/MouseExpressionMatrix_voxel_coronal_maskcoronal_log2_grouped_imputed_labelled_scaled.csv'
 outfile <- 'data/isocortex/MouseExpressionMatrix_voxel_coronal_maskcoronal_log2_grouped_imputed_labelled_scaled.csv'
 
-df_mouse <- data.table::fread(infile, header = TRUE) %>% 
+data.table::fread(infile, header = TRUE) %>% 
   as_tibble() %>% 
   filter(Region11 == 'Isocortex') %>% 
+  data.table::fwrite(outfile)
+
+infile <- 'data/MouseExpressionMatrix_voxel_coronal_maskcoronal_log2_grouped_imputed_labelled.csv'
+outfile <- 'data/isocortex_scaled/MouseExpressionMatrix_voxel_coronal_maskcoronal_log2_grouped_imputed_labelled.csv'
+
+data.table::fread(infile, header = TRUE) %>% 
+  as_tibble() %>% 
+  filter(Region11 == 'Isocortex') %>% 
+  data.table::fwrite(outfile)
+
+infile <- 'data/HumanExpressionMatrix_samples_pipeline_abagen_labelled.csv'
+outfile <- 'data/isocortex_scaled/HumanExpressionMatrix_samples_pipeline_abagen_labelled.csv'
+
+data.table::fread(infile, header = TRUE) %>% 
+  as_tibble() %>% 
+  filter(Region16 %in% c("limbic lobe", 
+                         "frontal lobe", 
+                         "insula", 
+                         "occipital lobe",
+                         "parietal lobe", 
+                         "temporal lobe")) %>% 
+  filter(!(Region88 %in% c("claustrum",
+                           "dentate gyrus",
+                           "CA1 field",
+                           "CA2 field",
+                           "CA3 field",
+                           "CA4 field",
+                           "subiculum"))) %>% 
   data.table::fwrite(outfile)
 
