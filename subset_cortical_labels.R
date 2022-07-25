00cb5cfebe5fc415b966c6c62f168da689bdf755
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-07-25 15:40:52 -0400

    Cleaned up subtree files

--- subset_cortical_labels.R
+++ subset_cortical_labels.R
@@ -1,24 +1,29 @@
+#Packages
 suppressPackageStartupMessages(library(tidyverse))
 
-infile <- 'data/MouseExpressionMatrix_voxel_coronal_maskcoronal_log2_grouped_imputed_labelled_scaled.csv'
-outfile <- 'data/isocortex_v1/MouseExpressionMatrix_voxel_coronal_maskcoronal_log2_grouped_imputed_labelled_scaled.csv'
+#Output directory
+outdir='data/isocortex/'
 
-data.table::fread(infile, header = TRUE) %>% 
-  as_tibble() %>% 
-  filter(Region11 == 'Isocortex') %>% 
-  data.table::fwrite(outfile)
+#Create outdir if needed
+if (!dir.exists(outdir)) {
+  dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
+}
 
+#Input and output mouse files
 infile <- 'data/MouseExpressionMatrix_voxel_coronal_maskcoronal_log2_grouped_imputed_labelled.csv'
-outfile <- 'data/isocortex_v2/MouseExpressionMatrix_voxel_coronal_maskcoronal_log2_grouped_imputed_labelled.csv'
+outfile <- file.path(outdir, basename(infile))
 
+#Filter mouse input matrix for isocortical labels and write out
 data.table::fread(infile, header = TRUE) %>% 
   as_tibble() %>% 
   filter(Region11 == 'Isocortex') %>% 
   data.table::fwrite(outfile)
 
+#Input and output human files
 infile <- 'data/HumanExpressionMatrix_samples_pipeline_abagen_labelled.csv'
-outfile <- 'data/isocortex_v2/HumanExpressionMatrix_samples_pipeline_abagen_labelled.csv'
+outfile <- file.path(outdir, basename(infile))
 
+#Filter human input matrix for cortical labels and write out
 data.table::fread(infile, header = TRUE) %>% 
   as_tibble() %>% 
   filter(Region16 %in% c("limbic lobe", 
