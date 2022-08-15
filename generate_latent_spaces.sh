02303dc557242544c3a41a0293646bbeb8a058ab
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-08-15 14:01:32 -0400

    Added vars for hyperparameters

--- generate_latent_spaces.sh
+++ generate_latent_spaces.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 # ----------------------------------------------------------------------------
-# generate_latent_spaces
+# generate_latent_spaces.sh
 # Author: Antoine Beauchamp
 #
 # This script generates 500 gene expression latent spaces by repeatedly 
@@ -17,13 +17,21 @@ outdir=data/MLP_outcomes/
 # outdir=data/MLP_outcomes_isocortex/
 
 if [ ! -d "$outdir" ]; then
-	mkdir "$outdir"
+	mkdir -p "$outdir"
 fi
 
+niterations=200
 nunits=200
 L2=0.0
+nepochs=150
+totalsteps=200
+learningrate=1e-05
+optimizer=AdamW
+confusionmatrix=false
+voxeltransform=true
+integratedgrads=true
 
-for i in {1..500}
+for i in $(seq 1 $niterations);
 do
  echo "Iteration $i"
 
@@ -35,15 +43,14 @@ do
 	 --humandata region88 \
 	 --nunits $nunits \
 	 --L2 $L2 \
-	 --nepochs 25 \
-	 --totalsteps 50 \
-	 --learningrate 0.01 \
-	 --confusionmatrix false \
-	 --voxeltransform true \
+	 --nepochs $nepochs \
+	 --totalsteps $totalsteps \
+     --optimizer $optimizer \
+	 --learningrate $learningrate \
+	 --confusionmatrix $confusionmatrix \
+	 --voxeltransform $voxeltransform \
+     --integratedgrads $integratedgrads \
 	 --seed $i
-
-#  mv ${outdir}MLP_Region67_Layers3_Units${nunits}_L2${L2}_IntegratedGradients.csv \
-#      ${outdir}MLP_Region67_Layers3_Units${nunits}_L2${L2}_IntegratedGradients_$i.csv
 
  mv ${outdir}MLP_Region67_Layers3_Units${nunits}_L2${L2}_HumanProb_Region88.csv \
 	 ${outdir}MLP_Region67_Layers3_Units${nunits}_L2${L2}_HumanProb_Region88_$i.csv
@@ -56,12 +63,27 @@ do
 
  mv ${outdir}MLP_Region67_Layers3_Units${nunits}_L2${L2}_MouseTx_Region67.csv \
 	 ${outdir}MLP_Region67_Layers3_Units${nunits}_L2${L2}_MouseTx_Region67_$i.csv
+     
+ if [ $confusionmatrix = 'true' ];
+ then
+    mv ${outdir}MLP_ConfusionMatrix_Training_Region67_Layers3_Units${nunits}_L2${L2}.csv \
+    ${outdir}MLP_ConfusionMatrix_Training_Region67_Layers3_Units${nunits}_L2${L2}_$i.csv
+ fi
+     
+ if [ $voxeltransform = 'true' ];
+ then
+    mv ${outdir}MLP_Region67_Layers3_Units${nunits}_L2${L2}_MouseVoxelTx.csv \
+        ${outdir}MLP_Region67_Layers3_Units${nunits}_L2${L2}_MouseVoxelTx_$i.csv
 
-mv ${outdir}MLP_Region67_Layers3_Units200_L2${L2}_MouseVoxelTx.csv \
-	${outdir}MLP_Region67_Layers3_Units200_L2${L2}_MouseVoxelTx_$i.csv
-
-mv ${outdir}MLP_Region67_Layers3_Units200_L2${L2}_HumanVoxelTx.csv \
-	${outdir}MLP_Region67_Layers3_Units200_L2${L2}_HumanVoxelTx_$i.csv
+    mv ${outdir}MLP_Region67_Layers3_Units${nunits}_L2${L2}_HumanVoxelTx.csv \
+        ${outdir}MLP_Region67_Layers3_Units${nunits}_L2${L2}_HumanVoxelTx_$i.csv
+ fi
+ 
+ if [ $integratedgrads = 'true' ];
+ then
+     mv ${outdir}MLP_Region67_Layers3_Units${nunits}_L2${L2}_IntegratedGradients.csv \
+         ${outdir}MLP_Region67_Layers3_Units${nunits}_L2${L2}_IntegratedGradients_$i.csv
+ fi
 
 done
 
